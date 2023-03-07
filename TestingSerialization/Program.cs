3b5e809b89d8a2ae3287e1e2b0dba3f7a0f25b83
Srikanth Reddy Bojja <srikanth.bojja@sikka.ai>
2023-03-06 18:47:56 -0800

    initial commit

---
+++ TestingSerialization/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace SerializationExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read the JSON data from the file
+            string jsonString = File.ReadAllText("data.json");
+
+            // deserialize the JSON string to a Car object
+            Car car = JsonSerializer.Deserialize<Car>(jsonString);
+
+            // create a new Car object with some modified properties
+            Car newCar = new Car
+            {
+                Make = "Honda",
+                Model = "Civic",
+                Year = 2020,
+                Color = "Blue",
+                Features = new List<string> { "Navigation", "Sunroof" }
+            };
+
+            // serialize the new Car object to JSON
+            string newJsonString = JsonSerializer.Serialize(newCar);
+
+            // write the JSON data to a new file
+            File.WriteAllText("newdata.json", newJsonString);
+
+            Console.WriteLine("Writing to new file is also done");
+        }
+    }
+
+    // define classes to be serialized/deserialized
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public List<string> Features { get; set; }
+    }
+}
